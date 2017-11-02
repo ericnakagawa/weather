b23df1bb9a32541ab4234bfffbf9d45d06a2bd0c
Eric Nakagawa <ericnakagawa@gmail.com>
2017-11-02 11:02:33 +0900

    HTTPS

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class WeatherDisplay extends Component {
     console.log('componentDidMount() fired');
     const zip = this.props.zip;
     const URL =
-      'http://api.openweathermap.org/data/2.5/weather?q=' +
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
       zip +
       '&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial';
     fetch(URL).then(res => res.json()).then(json => {
